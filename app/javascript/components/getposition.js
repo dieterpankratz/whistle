dff28d661d3c9070f469157e55a743fdb94c464f
Dieter Pankratz <pankratz117@gmail.com>
2019-11-26 07:25:53 +0100

    add comments to getPosition function

--- app/javascript/components/getposition.js
+++ app/javascript/components/getposition.js
@@ -1,5 +1,10 @@
 function getPosition() {
+    // the navigator.geolocation returns a geolocation object that gives web content access
+    // to the location of the device
     if(navigator.geolocation) {
+      // getCurrentPosition is used to get current position of device. It has a call-back function
+      // as an argument. successPosition has a position object as its parameter. This position object
+      // will have all the details we require: coords & timestamp.
       navigator.geolocation.getCurrentPosition(successPosition);
     }
     else
