10da0df6a7310a74a1ccd63d8c81e1759935df34
Morgane Gedio <morgane.gedio@gmail.com>
2019-11-26 17:02:55 +0100

    create seed file for users & create a add contact page to create connection  & search function within this page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:connect]
 
+  def index
+    if params[:query].present?
+      @users = User.where("username ILIKE ?", "%#{params[:query]}%")
+    else
+      @users = User.all_except(current_user)
+    end
+  end
+
   def connect
     if current_user.connect(@user.id)
       respond_to do |format|
