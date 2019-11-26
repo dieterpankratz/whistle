c8b67a6c66745c55c1bb9e02eec3703ca5ff803b
Thomas Lacheze <thomas.lchz@gmail.com>
2019-11-26 17:22:54 +0100

    created the show page for the trip input form

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,30 @@
 class TripsController < ApplicationController
   def show
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  # def create
+  #   @trip = Trip.new(trip_params)
+
+  #   @trip.user = current_user
+  # end
+
+  # def update
+  #   set_trip
+  #   @trip.update(trip_params)
+  #   redirect_to trip_path(@trip)
+  # end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:start_point, :end_point)
+  end
 end
