b4072f2e26a818b51a48d867176e1455140155cd
chloe barreau <cbarreau9@gmail.com>
2019-11-28 16:45:28 +0100

    implementing twilio sms and updating alert controller accordingly

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -4,11 +4,6 @@ class AlertsController < ApplicationController
     authorize @alert
   end
 
-  # def new
-  #   @alert = Alert.new
-  #   authorize @alert
-  # end
-
   def create
     @alert = Alert.new
     @trip = Trip.find(params[:trip_id])
@@ -21,34 +16,12 @@ class AlertsController < ApplicationController
       @alert.kind = "whistle"
       # set lat and lng
     end
-
-    # if params[:kind] == whistle
-    #   twiilio is this
-    # else
-    #   twilio is thate
-    # end
-
     authorize @alert
-    # SEND TO ALL USERS
-    # 1. create alert
     if @alert.save
-    # 2. get current user.. and find their connections
-      current_user.connections.each do |connection|
-        # 3. for EACH connection
-        # 4. -> send alert via text(twilio)
-        # send text through twillio
-      end
+      SendTwilioMessage.new(@alert).send_alert
+    end
       redirect_to alert_path(@alert)
     end
-
-
-
-    # if @trip.save!
-    #   redirect_to trip_path(@trip)
-    # else
-    #   render :new
-    # end
-    # authorize @trip
   end
 
   def edit
@@ -74,5 +47,3 @@ class AlertsController < ApplicationController
     params.require(:alert).permit(:kind, :trip_id, :lat, :long, :user_id)
   end
 end
-
-# alert => whistle, share
