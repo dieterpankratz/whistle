3dd04ae238850d205cb8f253ce8693e93f2ebfae
Thomas Lacheze <thomas.lchz@gmail.com>
2019-11-28 12:34:00 +0100

    geocoder w google api

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  geocoded_by :start_point, :end_point
+  after_validation :geocode, if: :will_save_change_to_start_point?
+  after_validation :geocode, if: :will_save_change_to_end_point?
 end
