46364c62aa54c5869e532176baaf6af5ba902f5c
Dieter Pankratz <pankratz117@gmail.com>
2019-11-29 07:10:52 +0100

    deleted useless comments

--- app/javascript/components/showRoute.js
+++ app/javascript/components/showRoute.js
@@ -61,40 +61,4 @@ function drawPath() {
   });
 }
 
-
-
-
-
-
-//   drawPath();
-// }
-
-// function drawPath() {
-//   var directionsService = new google.maps.DirectionsService();
-//   var directionsRenderer = new google.maps.DirectionsRenderer();
-
-
-
-//   var tripMap = new google.maps.Map(document.getElementById("tripMap"), mapOptions)
-//   directionsRenderer.setMap(tripMap);
-
-//   var poly = new google.maps.Polyline({strokeColor:"#085F63", strokeWeight:4})
-
-  // var request = {
-  //   origin: new google.maps.LatLng(points[0].lat, points[0].long),
-  //   destination: new google.maps.LatLng(points[1].lat, points[1].long),
-  //   travelMode: 'WALKING'
-  // };
-
-  // directionsService.route(request, function(response, status){
-  //   if (status == google.maps.DirectionsStatus.OK) {
-  //     new google.maps.DirectionsRenderer({
-  //       map: tripMap,
-  //       polylineOptions: poly,
-  //       directions: response
-  //     });
-  //   }
-  // });
-
-
 export { showRoute };
