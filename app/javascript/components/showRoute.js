c0c6e9f748a6de0075130335ee88c6194e7b4bdf
Dieter Pankratz <pankratz117@gmail.com>
2019-11-29 10:47:37 +0100

    fixed bugs

--- app/javascript/components/showRoute.js
+++ app/javascript/components/showRoute.js
@@ -3,7 +3,7 @@ let tripMap;
 function showRoute() {
   // wait until the content of the page is loaded:
   // points[0] is current location. points[1] is destination.
-  points[0].lat = parseFloat(document.querySelector("#start_lat").innerHTML);
+  points[0].lat = document.querySelector("#start_lat").innerHTML;
   points[0].long = document.querySelector("#start_long").innerHTML;
   points[1].lat = document.querySelector("#end_lat").innerHTML;
   points[1].long = document.querySelector("#end_long").innerHTML;
@@ -60,41 +60,5 @@ function drawPath() {
     }
   });
 }
-
-
-
-
-
-
-//   drawPath();
-// }
-
-// function drawPath() {
-//   var directionsService = new google.maps.DirectionsService();
-//   var directionsRenderer = new google.maps.DirectionsRenderer();
-
-
-
-//   var tripMap = new google.maps.Map(document.getElementById("tripMap"), mapOptions)
-//   directionsRenderer.setMap(tripMap);
-
-//   var poly = new google.maps.Polyline({strokeColor:"#085F63", strokeWeight:4})
-
-  // var request = {
-  //   origin: new google.maps.LatLng(points[0].lat, points[0].long),
-  //   destination: new google.maps.LatLng(points[1].lat, points[1].long),
-  //   travelMode: 'WALKING'
-  // };
-
-  // directionsService.route(request, function(response, status){
-  //   if (status == google.maps.DirectionsStatus.OK) {
-  //     new google.maps.DirectionsRenderer({
-  //       map: tripMap,
-  //       polylineOptions: poly,
-  //       directions: response
-  //     });
-  //   }
-  // });
-
 
 export { showRoute };
