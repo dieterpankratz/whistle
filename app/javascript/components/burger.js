f0fa0a09ec9b09b0ebfe648fc0cc7c48ca382032
Morgane Gedio <morgane.gedio@gmail.com>
2019-12-01 20:00:21 +0100

    modify burger animation and display only if user logged in

--- app/javascript/components/burger.js
+++ app/javascript/components/burger.js
@@ -1,21 +1,10 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
 
-  //Toggle Nav
-    burger.addEventListener('click', ()=>{
+  burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-          navLinks.forEach((link,index) => {
-              if(link.style.animation){
-                link.style.animation=''
-              }else{
-              link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`
-              }
-
-  });
-          //burger animation
-          burger.classList.toggle('is-open');
+    burger.classList.toggle('is-open');
   })
 
 }
