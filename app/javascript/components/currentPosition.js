c82744bff56bb9f92bf07c1a41de6be5f702163f
Morgane Gedio <morgane.gedio@gmail.com>
2019-12-04 09:57:20 +0100

    fix the whistle marker - not appear after trip is shared

--- app/javascript/components/currentPosition.js
+++ app/javascript/components/currentPosition.js
@@ -26,7 +26,7 @@ const addUserPosition = (map, latitude, longitude) => {
   const currentLatLong = new google.maps.LatLng(latitude, longitude);
 
 
-  const currPushPin = 'https://res.cloudinary.com/frijolyfrailejon/image/upload/c_scale,w_40/v1575394190/Group_2_zdizst.png'
+  const currPushPin = 'https://res.cloudinary.com/frijolyfrailejon/image/upload/c_scale,w_40/v1575447337/Group_1_1_iqbse2.png'
   const currentMarker = new google.maps.Marker({
     position: currentLatLong,
     map: map,
