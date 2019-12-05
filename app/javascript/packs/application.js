26da8edb1657a47a97d97793ce52baebc4663e33
Morgane Gedio <morgane.gedio@gmail.com>
2019-12-05 16:59:45 +0100

    add pop up messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import {autocomplete} from '../components/autocomplete'
 import  createMap  from '../components/createMap';
 import { showRoute } from '../components/showRoute';
 import { alertDisplay } from '../components/alertDisplay'
+import Swal from 'sweetalert2';
+
+
 
 
 // trips show
@@ -67,6 +70,66 @@ document.addEventListener('DOMContentLoaded', function() {
 const alertMap = document.getElementById("alertMap")
 if (alertMap) {
   alertDisplay(alertMap)
+}
+
+
+
+const showTripForm = document.querySelector('.show-trip-form');
+if(showTripForm) {
+  showTripForm.addEventListener('submit', event => {
+    event.preventDefault();
+     Swal.fire({
+        position: 'center',
+        title: 'Itinerary shared with your friends !',
+        showConfirmButton: false,
+        height: 500,
+        padding: 100,
+        timer: 1500
+    })
+     setTimeout(() => {
+      showTripForm.submit();
+
+     }, 1500)
+  });
+
+}
+
+const alertWhistleForm = document.querySelector('.alert-whistle-form');
+if(alertWhistleForm) {
+  alertWhistleForm.addEventListener('submit', event => {
+    event.preventDefault();
+     Swal.fire({
+        position: 'center',
+        title: 'Looking for friends nearby !',
+        showConfirmButton: false,
+        height: 500,
+        padding: 100,
+        timer: 2000
+    })
+     setTimeout(() => {
+      alertWhistleForm.submit();
+
+     }, 2000)
+  });
+}
+
+const safeForm = document.querySelector('.safe-form');
+if(safeForm) {
+  safeForm.addEventListener('submit', event => {
+    event.preventDefault();
+     Swal.fire({
+        position: 'center',
+        title: "Happy that you're safe!",
+        showConfirmButton: false,
+        height: 500,
+        padding: 100,
+        timer: 1500
+    })
+     setTimeout(() => {
+      safeForm.submit();
+
+     }, 1500)
+  });
 }
 
 
